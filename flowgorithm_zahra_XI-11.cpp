3c8957e14aa65b134b4b29b7c99f46c1156891d9
zahra1802 <141710450+zahra1802@users.noreply.github.com>
2023-10-10 09:37:38 +0700

    Add files via upload

---
+++ flowgorithm_zahra_XI-11.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+// Headers
+string toString (double);
+int toInt (string);
+double toDouble (string);
+
+int main() {
+    int alas;
+    int tinggi;
+    double luas;
+
+    cout << "Masukkan alas : ";
+    cin >> alas;
+    cout << "Masukkan tinggi : ";
+    cin >> tinggi;
+    luas = alas*tinggi*0.5;
+    cout << luas << endl;
+    return 0;
+}
+
+// The following implements type conversion functions.
+string toString (double value) { //int also
+    stringstream temp;
+    temp << value;
+    return temp.str();
+}
+
+int toInt (string text) {
+    return atoi(text.c_str());
+}
+
+double toDouble (string text) {
+    return atof(text.c_str());
+}
+
